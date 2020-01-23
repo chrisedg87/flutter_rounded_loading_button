e3a207ff3c44748ba77e1a62b910b187be95bc7b
Chris Edgington <chrisedg87@me.com>
2020-01-23 20:27:11 +0000

    Updated version

--- test/rounded_loading_button_test.dart
+++ test/rounded_loading_button_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rounded_loading_button/rounded_loading_button.dart';
+// import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 void main() {
   // test('adds one to input values', () {
