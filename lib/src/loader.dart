85bf861061938213933bf575c8e3d7ebdade79e7
Gianluca Bettega <gian_bettega@hotmail.com>
2023-06-18 19:31:57 -0300

    future custom indicator

--- lib/src/loader.dart
+++ lib/src/loader.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 
+/// Circular progress indicator
 class Loader extends StatelessWidget {
   const Loader({
     super.key,
     required this.loaderStrokeWidth,
     required this.loaderSize,
     required this.iconsColor,
+    this.progressIndicator,
   });
   final double loaderStrokeWidth;
   final double loaderSize;
   final Color iconsColor;
+  final Widget? progressIndicator;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: loaderSize,
       width: loaderSize,
-      child: CircularProgressIndicator(
+      child: progressIndicator ?? CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(iconsColor),
         strokeWidth: loaderStrokeWidth,
       ),
