325eda0ab20a4012a452d420e9bca5d2345084fa
Nana Kwame <aduhenry9@gmail.com>
2023-06-29 12:51:45 +0000

    Added animation to borderside properties

--- lib/rounded_loading_button.dart
+++ lib/rounded_loading_button.dart
@@ -197,10 +197,12 @@ class RoundedLoadingButtonState extends State<RoundedLoadingButton>
     final _btn = ButtonTheme(
       shape: RoundedRectangleBorder(
         borderRadius: _borderAnimation.value,
-        side: widget.borderSide?.copyWith(
-              color: _borderColorAnimation.value,
-              width: _borderWidthAnimation.value,
-            ) ??
+        side: widget.borderSide
+            // ?.copyWith(
+            //   color: _borderColorAnimation.value,
+            //   width: _borderWidthAnimation.value,
+            // )
+            ??
             BorderSide.none,
       ),
       disabledColor: widget.disabledColor,
